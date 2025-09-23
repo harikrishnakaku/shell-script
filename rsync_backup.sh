f04c89eba9ec746430e0c79e972cfdc1945b630f
harikrishna <harikrishna.kaku999@gmail.com>
2025-09-23 12:47:22 +0530

    rsync_backup

---
+++ rsync_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 	#Shebang Line: Tells the system to use the bash shell to run this script
+
+SOURCE="/data/"		#The source Directory that you want to backup
+DEST="root@192.168.49.238:/backup/data"	#The destination path where backup will be stored (remote server with IP Address)
+LOG_FILE="/var/log/backup.log"	#File where the backup logs (success, error, details) will be written 
+
+#run rsync command to synchronize files
+rsync -avz --delete "$SOURCE" "$DEST" >> "$LOG_FILE" 2>&1
+
+if [ "$?" -eq 0 ]; then
+	echo "Backup Successful on $(date)" >> "$LOG_FILE"	#If rsync exit status is 0, it means backup successfully commpleted
+else
+	echo "Backup failed on $(date)" >> "$LOG_FILE"		#If rsync exit status is not 0, it means backup failed.
+fi
